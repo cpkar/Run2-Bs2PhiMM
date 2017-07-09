2ec4a360358f38ddc0b8f4d3e50d8a88efcbc0ff
nsahoo <niladribihari.sahoo@cern.ch>
2017-07-09 14:04:27 +0200

    fixed config file to run on unfiltered mc

--- BsToPhiMuMu/python/bstophimumu_2016_mc_KeepGenOnly.py
+++ BsToPhiMuMu/python/bstophimumu_2016_mc_KeepGenOnly.py
@@ -1,6 +1,7 @@
-
+#=======================================
 # @author: N.Sahoo <nsahoo@cern.ch>
 # @date: 2017-06-30
+#=======================================
 
 #-------------
 #cmssw config
@@ -8,11 +9,13 @@
 import FWCore.ParameterSet.Config as cms
 from bstophimumu_2016_cfi_may8 import process
 
-process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(-1) )
+process.options = cms.untracked.PSet(SkipEvent = cms.untracked.vstring('ProductNotFound'))
+process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1000) )
 
 process.source = cms.Source("PoolSource",
                             fileNames = cms.untracked.vstring(
      '/store/user/nsahoo/PYTHIA8_Bs2PhiMuMu_GenOnly_13TeV_NoFilter/crab_edm_Bs2PhiMuMu_MC_GenOnly_13TeV_NoFilter/170630_082400/0000/PYTHIA8_Bs2PhiMuMu_TuneCUEP8M1_13TeV_GenOnly_NoFilter_1.root',
+##'/store/user/dsahoo/PYTHIA8_Bs2PhiMuMu_GenOnly_13TeV_NoFilter/crab3_Bs2PhiMuMu_MC_GenOnly_13TeV_NoFilter_new/170701_212745/0000/PYTHIA8_Bs2PhiMuMu_TuneCUEP8M1_13TeV_GenOnly_NoFilter_1.root',
                            )
                         )
 
